13f34e0cd0a9c0b7cb5f79ec5adf8cd7c49fd493
caioandrade42 <caioandradesje@gmail.com>
2025-05-14 23:28:00 -0300

    rota cadastro de usuario

--- web2-laravel/app/Http/Controllers/UserController.php
+++ web2-laravel/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -9,18 +10,43 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function cadastrarUsuario(Request $request){
 
+    public function formCadastrarUsuario(){
+        return view('users.form');
+    }
+    public function cadastrarUsuario(Request $request){
+        try {
+            $validator = Validator::make($request->all(), [
+                'nome' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email',
+                'data_nascimento' => 'required|date:Y-m-d',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+            if ($validator->fails()) {
+                Log::error('UserController::cadastrarUsuario::validator '.$validator->errors());
+                return redirect('/users/cadastrar')->with('errors', 'Verifique os campos e tente novamente.');
+            }
+            $user = new User();
+            $user->create([
+                'name' => $request->nome,
+                'email' => $request->email,
+                'data_nascimento' => $request->data_nascimento,
+                'password' => bcrypt($request->password),
+            ]);
+        }catch (\Exception $e){
+            Log::error('UserController::cadastrarUsuario '.$e->getMessage());
+            return redirect('/users/cadastrar')->with('error', 'Erro ao criar usuário.');
+        }
     }
 
     public function login(Request $request){
         try {
-            $validor = Validator::make($request->all(), [
+            $validator = Validator::make($request->all(), [
                 'email' => 'required|email',
                 'password' => 'required'
             ]);
-            if ($validor->fails()) {
-                Log::error('UserController::login::validator '.$validor->errors());
+            if ($validator->fails()) {
+                Log::error('UserController::login::validator '.$validator->errors());
                 return response()->json(['verifique os dados e tente novamente',401]);
             }
             Auth::attempt($request->only('email', 'password'));
@@ -32,7 +58,7 @@ class UserController extends Controller
         }
     }
 
-    public function logout(Request $request){
+    public function logout(){
         try {
             Auth::logout();
             request()->session()->invalidate();
--- web2-laravel/routes/web.php
+++ web2-laravel/routes/web.php
@@ -18,17 +18,18 @@ Route::prefix('/livros', )->group(function () {
     Route::get('/', [LivroController::class, 'exibirLivros']);
 });
 
+route::post('/login', [UserController::class, 'login']);
 Route::prefix('/users')->group(function () {
     route::post('/cadastrar', [UserController::class, 'cadastrarUsuario']);
-    route::post('/login', [UserController::class, 'login']);
+    route::get('/cadastrar', [UserController::class, 'formCadastrarUsuario']);
     route::get('/logout', [UserController::class, 'logout'])->middleware('auth');
 });
 
 Route::get('/filmes', [FilmeController::class, 'exibirFilmes']);
 Route::prefix('/filmes')->middleware('auth')->group(function () {
-    Route::get('/filmes/criar', [FilmeController::class, 'criarFilme']);
-    Route::post('/filmes/criar', [FilmeController::class, 'armazenarFilme']);
-    Route::post('/filmes/editar', [FilmeController::class, 'editarFilme']);
-    Route::post('/filmes/atualizar', [FilmeController::class, 'atualizarFilme']);
-    Route::post('/filmes/excluir', [FilmeController::class, 'excluirFilme']);
+    Route::get('/criar', [FilmeController::class, 'criarFilme']);
+    Route::post('/criar', [FilmeController::class, 'armazenarFilme']);
+    Route::post('/editar', [FilmeController::class, 'editarFilme']);
+    Route::post('/atualizar', [FilmeController::class, 'atualizarFilme']);
+    Route::post('/excluir', [FilmeController::class, 'excluirFilme']);
 });
